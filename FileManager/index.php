c39a814adf93121124bff272e22a2d5595f9962c
nizhrgorodtsev <30364030+nizhrgorodtsev@users.noreply.github.com>
2017-12-27 16:20:31 +0200

    up index.php
    змінив пошук першої крапки на пошук останньої крапки. Додав іконки для різних типів файлів.

--- FileManager/index.php
+++ FileManager/index.php
@@ -28,10 +28,9 @@ array_splice($dir, 0, 2); // видаляємо крапки в масиві
 $files = array();
 $folders = array();
 foreach($dir as $key => $value){
-	if(strstr($value, '.')) array_push ($files, $value); //strstr() - шукає крапку, якщо крапка є array_push() - додає елемент в кінець попередньо створеного масива
+	if(strrchr($value, '.')) array_push ($files, $value); //strrchr() - шукає крапку, якщо крапка є array_push() - додає елемент в кінець попередньо створеного масива
 	else array_push ($folders, $value);
 }
-
 ?>
 
 <div class="jumbotron text-center">
@@ -50,8 +49,7 @@ foreach($dir as $key => $value){
     </thead>
     <tbody>
 	
-	<?php 
-	
+	<?php //виводим іконки папок, назви папок, ссилки з гет параметрами для роботи вкладеності
 	foreach($folders as $key => $value):?>
       <tr>
         <td><img src="img/folders-icon.png"><a href="?way=<?php echo $way.'/'.$value ?>"><?php echo $value ?></a></td>
@@ -60,15 +58,33 @@ foreach($dir as $key => $value){
       </tr>	
 	<?php endforeach ?>
 	
-	<?php
+	<?php //виводим іконки різноманітних типів файлів, назви файлів
 	foreach($files as $key => $value):?>
 		<tr>
-	<?php if(strstr($value, '.') == '.php'):?>
+	<?php if(strrchr($value, '.') == '.php'):?>
         <td><?php echo '<img src="img/php_icon.png">'.$value ?></td>
+	<?php elseif(strrchr($value, '.') == '.html'):?>
+        <td><?php echo '<img src="img/html_icon.png">'.$value ?></td>
+	<?php elseif(strrchr($value, '.') == '.zip'):?>
+        <td><?php echo '<img src="img/zip-icon.jpg">'.$value ?></td>		
+	<?php elseif(strrchr($value, '.') == '.xml'):?>
+        <td><?php echo '<img src="img/xml-icon.png">'.$value ?></td>
+	<?php elseif(strrchr($value, '.') == '.sql'):?>
+        <td><?php echo '<img src="img/sql-icon.png">'.$value ?></td>
+	<?php elseif(strrchr($value, '.') == '.css'):?>
+        <td><?php echo '<img src="img/css-icon.jpg">'.$value ?></td>
+	<?php elseif(strrchr($value, '.') == '.js'):?>
+        <td><?php echo '<img src="img/js-icon.png">'.$value ?></td>		
+	<?php elseif(strrchr($value, '.') == '.jpg'):?>
+        <td><?php echo '<img src="img/jpg-icon.jpg">'.$value ?></td>
+	<?php elseif(strrchr($value, '.') == '.png'):?>
+        <td><?php echo '<img src="img/png-icon.jpg">'.$value ?></td>	
+	<?php elseif(strrchr($value, '.') == '.gif'):?>
+        <td><?php echo '<img src="img/gif-icon.jpg">'.$value ?></td>			
 	<?php else: ?>
         <td><?php echo '<img src="img/files-icon.png">'.$value ?></td>
 	<?php endif ?>
-        <td><?php echo strstr($value, '.') ?></td>
+        <td><?php echo strrchr($value, '.') ?></td>
         <td><?php echo filesize('/xampp/htdocs/nizhegorodtsev/FileManager/'.$way.'/'.$value)?> b</td>
       </tr>	
 	<?php endforeach ?>
